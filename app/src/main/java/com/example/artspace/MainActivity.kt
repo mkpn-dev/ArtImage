bebeb977c74449a7252ce6078f666685860ba3ed
makapaka <makapaka.saga@gmail.com>
2025-05-18 18:34:39 +0900

    first Commit

---
+++ app/src/main/java/com/example/artspace/MainActivity.kt
@@ -0,0 +1,141 @@
+package com.example.artspace
+
+import android.os.Bundle
+import android.view.Display.Mode
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.artspace.ui.theme.ArtSpaceTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ArtSpaceTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "No1! Burger",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ArtImage() {
+    Box(
+        modifier = Modifier
+            .padding(32.dp)
+            .shadow(4.dp, shape = RoundedCornerShape(8.dp))
+            .background(Color.White, shape = RoundedCornerShape(8.dp))
+            .padding(16.dp)
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.main_image),
+            contentDescription = "Main image",
+            modifier = Modifier
+                .fillMaxWidth()
+                .clip(RoundedCornerShape(4.dp))
+        )
+    }
+}
+
+@Composable
+fun ArtTitle(title: String) {
+    Text(
+        text = title,
+        style = MaterialTheme.typography.titleLarge.copy(fontSize = 28.sp),
+        modifier = Modifier.padding(top = 16.dp)
+    )
+}
+
+@Composable
+fun ArtDescription(description: String) {
+    Text(
+        text = description,
+        style = MaterialTheme.typography.bodyLarge,
+        modifier = Modifier.padding(top = 8.dp)
+    )
+}
+
+@Composable
+fun PreviousButton(modifier: Modifier = Modifier) {
+    Button(
+        onClick = { /*TODO*/ },
+        modifier = modifier
+    ) {
+        Text("Previous")
+    }
+}
+
+@Composable
+fun NextButton(modifier: Modifier = Modifier) {
+    Button(
+        onClick = { /*TODO*/ },
+        modifier = modifier
+    ) {
+        Text("Next")
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        ArtImage()
+        ArtTitle(title = name)
+        ArtDescription(description = "makapaka(2024)")
+        Spacer(modifier = Modifier.weight(1f))
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp, vertical = 8.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            PreviousButton(modifier = Modifier.weight(1f))
+            Spacer(modifier = Modifier.width(30.dp))
+            NextButton(modifier = Modifier.weight(1f))
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ArtSpaceTheme {
+        Greeting(name = "No1! Burger", modifier = Modifier)
+    }
+}
